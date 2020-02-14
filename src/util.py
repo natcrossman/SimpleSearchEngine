35434b761bb43c4f1ec8608a78c49889b04b268a
Nat <31453497+natcrossman@users.noreply.github.com>
2020-02-14 17:03:19 -0500

    adds util token class

--- src/util.py
+++ src/util.py
@@ -5,7 +5,9 @@
     shared by both indexing and query processing
 '''
 
-
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+import doc
 
 def isStopWord(word):
     ''' using the NLTK functions, return true/false'''
@@ -17,5 +19,28 @@ def stemming(word):
     ''' return the stem, using a NLTK stemmer. check the project description for installing and using it'''
 
     # ToDo
+#Here is an idea of a class, pls change or remove as you see fit. Cheers. 
+class tokens:
+    def token_for_doc(self, doc, type):
+        list_token = []
+        list_token = word_tokenize(doc.title + " " + doc.body)
+        list_token = [t.lower() for t in list_token]
+        return list_token
 
+    def token_for_query(self, raw_query, type):
+        list_token = []
+        list_token = word_tokenize(raw_query)
+        list_token = [t.lower() for t in list_token]
+        return list_token
+   
+    def stemming(word):
+        ''' return the stem, using a NLTK stemmer. check the project description for installing and using it'''
+        return stemmer.stem(word)  
 
+    def isStopWord(word):
+        ''' using the NLTK functions, return true/false'''
+        setOfStopWords = set(stopwords.words('stopwords'))
+        if word in setOfStopWords:
+            return True
+        else: 
+                return False
