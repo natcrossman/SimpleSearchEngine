8284a8fe8f6f5ed7a33c397cac06edf26e1ba803
plokm171 <AdamTSiens@gmail.com>
2020-02-19 15:47:44 -0500

    Did boolean Query and started vector query a little

--- src/query.py
+++ src/query.py
@@ -13,25 +13,56 @@ class QueryProcessor:
         self.raw_query = query
         self.index = index
         self.docs = collection
-        
-    def preprocessing(self):
+        self.tokenizer = util.Tokenizer()
+        self.processed_query = self.preprocessing(self.raw_query)
+
+    def preprocessing(self,raw_query):
         ''' apply the same preprocessing steps used by indexing,
             also use the provided spelling corrector. Note that
             spelling corrector should be applied before stopword
             removal and stemming (why?)'''
-
-        #ToDo: return a list of terms
-
+        return self.tokenizer.transpose_document_tokenized_stemmed_spelling(raw_query)
 
     def booleanQuery(self):
         ''' boolean query processing; note that a query like "A B C" is transformed to "A AND B AND C" for retrieving posting lists and merge them'''
-        #ToDo: return a list of docIDs
+        ''' This method would likely be faster due to the use of  hashes, but I wanted to do what was shown in the slides
+            from functools import reduce
+            postings = [set(self.index[w]) for w in self.processed_query]
+            postings.sort(key=len) # notice it is still smart to order by size 
+            return reduce(set.intersection,postings) 
+        '''
 
+        #### This may need to change based on how index is implemented ####
+        #### Postings should be a list of lists which contain only document ids ####
+        postings = [self.index[w] for w in self.processed_query]
+        postings.sort(key=len)
+        results= postings[0]
+        for p in postings[1:]:
+            intermediate=[]
+            i,j = 0,0
+            while i < len(results) and j < len(p): 
+                if results[i] < p[j]: 
+                    i += 1
+                elif results[i] > p[j]: 
+                    j+= 1
+                else: 
+                    intermediate.append(p[j]) 
+                    j += 1
+                    i += 1
+            results = intermediate
+        
+        return results
 
+            
     def vectorQuery(self, k):
         ''' vector query processing, using the cosine similarity. '''
         #ToDo: return top k pairs of (docID, similarity), ranked by their cosine similarity with the query in the descending order
         # You can use term frequency or TFIDF to construct the vectors
+        
+        #### This may need to change based on how index is implemented ####
+        #### Postings should be a list of lists which contains document ids and position ####
+        postings = [self.index[w] for w in self.processed_query]
+        
 
 
 
