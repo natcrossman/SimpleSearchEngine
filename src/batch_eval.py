79f9d2b179c1c9f26cf2aa807f209b05de8fda9b
Nat <31453497+natcrossman@users.noreply.github.com>
2020-02-24 15:10:17 -0500

    It has not been tested with the real query class I used synthetic data to test. Moreover not 100% sure I did the evaluation metrics right

--- src/batch_eval.py
+++ src/batch_eval.py
@@ -43,6 +43,7 @@ def getRandomQuery(queryFile,numberOfQueries):
 
     return dictOfQueryID
 
+
 def getResultsFrom_QrelsFile(listOfQueryRelsMaping,dictOfQuery):
     dictOfResult = collections.defaultdict(list)
     for query in listOfQueryRelsMaping:
@@ -54,38 +55,36 @@ def getResultsFrom_QrelsFile(listOfQueryRelsMaping,dictOfQuery):
         
 def eval():
     k               = 10 # k the number of top k pairs of (docID, similarity) to get from vectorQuery
-    yTrue           = []
-    yScore          = []
     indexFile       = sys.argv[1]
     queryText       = sys.argv[2]
     qrelsText       = sys.argv[3]
-    boolQueryDict   = []
-    vectorQueryDict = []
+    dictOfQuery     = {}
+    dictQrelsText   = {}
+    boolQueryDict   = {}
+    vectorQueryDict = {}
     docCollection   = CranFile('src/CranfieldDataset/cran.all')
     NDCGScoreBool   = []
     numberOfQueries = sys.argv[4]
     NDCGScoreVector = []
-
+    
 
     #Loads Files 
     listOfQueryRelsMaping   = readFile(qrelsText)
-    listOFQuery             = readFile(queryText)
+    queryFile   = loadCranQry(queryText)
+
+    #Data Need
+    dictOfQuery = getRandomQuery(queryFile,numberOfQueries)
+    dictQrelsText =  getResultsFrom_QrelsFile(listOfQueryRelsMaping, dictOfQuery)
+
+
 
     #Loads queryFile and create dict of random N querys (dict = {Qid:text})
-    queryFile   = loadCranQry(listOFQuery)
-    dictOfQuery = getRandomQuery(queryFile,numberOfQueries)
+    
 
     #Load invertedIndexer Object from file Not sure if need?
     # invertedTempIndexer = InvertedIndex()
     # invertedIndexer = invertedTempIndexer.loadData(indexFile)
-    testDataForBool ={1: [12, 14, 78, 141, 486, 746, 172, 573, 1003, 1147, 110, 252, 315, 329], 
-    2: [12, 14, 51, 82, 172, 374, 625, 700, 746, 792, 798, 810, 870, 1089], 
-    4: [344, 625, 1072, 5, 181, 399, 485, 542, 584, 144, 349, 579] 
-    }
-    testDataForVector ={1: [[12, 0.539],[14, 0.44], [25, 0.17], [13, 0.164] ,[51, 0.119],[29, 0.105]], 
-    2: [[12, 0.798],[ 14, 0.487 ], [29, 0.119], [16, 0.108], [51, 0.088], [25, 0.079], [47, 0.069] ], 
-    4: [[5, 0.234], [13, 0.13], [77, 0.118], [15, 0.11], [9, 0.096], [144, 0.082], [90, 0.08], [6, 0.075]] }
-    
+
     for qid, queryText in dictOfQuery.items():
         queryProcessor = QueryProcessor(queryText,indexFile,docCollection.docs) # need 
 
@@ -95,7 +94,37 @@ def eval():
         dictOfDocIDAndSimilarity = queryProcessor.vectorQuery(k) # data need to look like k=3 [[625,0.8737006126353902],[401,0.8697643788341478],[943,0.8424991316663082]]
         vectorQueryDict[qid] = dictOfDocIDAndSimilarity
 
+        #For Boolean part
+        yTrue           = []
+        yScore          = []
+        for qID, listOfDocId in boolQueryDict.items():
+            for docID in listOfDocId:
+                yTrue.append(1)
+                if docID in dictQrelsText[qID]:
+                    yScore.append(1)
+                else:
+                    yScore.append(0)
+        yScore.sort(reverse=True)    
+        NDCGScoreBool.append(metrics.ndcg_score(yTrue[:10], yScore[:10], 10, "exponential"))
 
+
+        #For Vector part
+        yTrue           = []
+        yScore          = []
+        for qID, listOfDocId_and_score in vectorQueryDict.items():
+            for docIdAndScore in listOfDocId_and_score:
+                yScore.append(float(docIdAndScore[1]))
+                if docID in dictQrelsText[qID]:
+                     yTrue.append(1)
+                else:
+                     yTrue.append(0)
+        yScore.sort(reverse=True) 
+        NDCGScoreVector.append(metrics.ndcg_score(yTrue[:10], yScore[:10], 10, "exponential"))
+
+    vectorAvg = avg(NDCGScoreVector)
+    BoolAvg = avg(NDCGScoreBool)
+
+    PVALUETHING = stats.ttest_ind(BoolAvg,vectorAvg)
 
     #loop through vectorQueryDict add 0 or 1 to yScore and add 1 to yTrue
     #NDCG_Score = metrics.ndcg_score(yScore[:10], yTrue[:10], 10, "exponential")
@@ -109,13 +138,5 @@ def eval():
 
 
 if __name__ == '__main__':
-    #eval()
-    listOfQueryRelsMaping   = readFile("src/CranfieldDataset/qrels.text")
-    listOFQuery             ="src/CranfieldDataset/query.text" 
+    eval()
 
-    queryFile   = loadCranQry(listOFQuery)
-
-    dictOfQuery = getRandomQuery(queryFile,10)
-
-    d =  getResultsFrom_QrelsFile(listOfQueryRelsMaping, dictOfQuery)
-    print(d)
